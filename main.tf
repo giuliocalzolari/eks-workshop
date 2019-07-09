68da574576ec8ad6461ddb425062442bf322b20d
Giulio Calzolari <giuliocalzolari@users.noreply.github.com>
2019-07-09 18:21:46 +0200

    clean up old modules

--- main.tf
+++ main.tf
@@ -36,26 +36,11 @@ resource "aws_security_group" "all_worker_mgmt" {
   }
 }
 
-# module "vpc" {
-#   source         = "terraform-aws-modules/vpc/aws"
-#   version        = "1.60.0"
-#   name           = "eks-vpc"
-#   cidr           = "10.0.0.0/16"
-#   azs            = ["${data.aws_availability_zones.available.names}"]
-#   public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-
-#   tags = {
-#     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#   }
-
-#   public_subnet_tags = {
-#     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#   }
-# }
 
 
 module "vpc" {
   source         = "terraform-aws-modules/vpc/aws"
+  version        = "2.7.0"
   name           = "eks-vpc"
   cidr           = "10.0.0.0/16"
   azs            = data.aws_availability_zones.available.names
@@ -76,44 +61,16 @@ module "vpc" {
 #   name = "/aws/eks/${local.cluster_name}/cluster"
 # }
 
-# module "eks-cluster" {
-#   source                                   = "terraform-aws-modules/eks/aws"
-#   config_output_path                       = "kubeconfig/"
-#   write_aws_auth_config                    = false
-#   cluster_name                             = "${local.cluster_name}"
-#   subnets                                  = ["${module.vpc.public_subnets}"]
-#   vpc_id                                   = "${module.vpc.vpc_id}"
-#   cluster_enabled_log_types                = ["api", "authenticator", "controllerManager", "scheduler"]
-#   worker_group_count                       = 0
-#   worker_group_launch_template_mixed_count = 1
-
-#   worker_groups_launch_template_mixed = [
-#     {
-#       name                 = "minion-1"
-#       instance_type        = "t2.small"
-#       asg_max_size         = 3
-#       asg_desired_capacity = 3
-#       public_ip            = true
-#       ebs_optimized        = false
-#     },
-#   ]
-
-#   tags = {
-#     environment = "gc"
-#   }
-
-#   worker_additional_security_group_ids = ["${aws_security_group.all_worker_mgmt.id}"]
-#   map_accounts                         = "${var.map_accounts}"
-#   map_accounts_count                   = "${var.map_accounts_count}"
-# }
 
 module "eks-cluster" {
-  source = "terraform-aws-modules/eks/aws"
-  # write_kubeconfig          = false
+  source                    = "terraform-aws-modules/eks/aws"
+  version                   = "1.6.0"
+  write_kubeconfig          = true
   write_aws_auth_config     = false
   cluster_name              = local.cluster_name
   subnets                   = module.vpc.public_subnets
   vpc_id                    = module.vpc.vpc_id
+  cluster_version           = "1.12"
   cluster_enabled_log_types = ["api", "authenticator", "controllerManager", "scheduler"]
 
 
@@ -126,7 +83,6 @@ module "eks-cluster" {
       ebs_optimized        = false
     }
   ]
-
 
   tags = {
     environment = "gc"
